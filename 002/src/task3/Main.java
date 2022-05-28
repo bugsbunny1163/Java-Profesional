20cb878e981518356cadfd21d17a77ea6c7b30c4
Volodymyr <bugsbunny1163@gmail.com>
2022-05-28 11:53:37 +0300

    002 task 3 done

--- 002/src/task3/Main.java
+++ 002/src/task3/Main.java
@@ -1,7 +1,27 @@
 package task3;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        LinkedList<Integer> list = getIntegerList();
+        getMinimum(list);
+    }
 
+    private static void getMinimum(LinkedList<Integer> list) {
+        System.out.println(Collections.min(list));
+    }
+
+    private static LinkedList<Integer> getIntegerList() {
+        LinkedList<Integer> list = new LinkedList<>();
+        System.out.println("input length of list");
+        Scanner scanner= new Scanner(System.in);
+        int listLength = scanner.nextInt();
+        for (int i = 0; i < listLength; i++) {
+            list.add(scanner.nextInt());
+        }
+        return list;
     }
 }
