0af781e2f1b966a71debf3d60517cf2af26e1289
Volodymyr <bugsbunny1163@gmail.com>
2022-06-01 13:09:23 +0300

    003 4 done

--- 003/src/task4/Main.java
+++ 003/src/task4/Main.java
@@ -1,4 +1,48 @@
 package task4;
 
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) {
+        String filePath = "003\\src\\task4\\textFile.txt";
+
+        try {
+            File file = new File(filePath);
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream(filePath)));
+            bufferedWriter.write("some info1");
+            bufferedWriter.newLine();
+            bufferedWriter.write("some info2");
+            bufferedWriter.newLine();
+            bufferedWriter.write("some info3");
+            bufferedWriter.newLine();
+            bufferedWriter.write("some info4");
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+            String text;
+            while ((text =bufferedReader.readLine())!=null){
+                System.out.println(text);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
