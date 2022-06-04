638116d548d9e81cb035c302ef9c1cec4b7fad5e
Volodymyr <bugsbunny1163@gmail.com>
2022-06-04 11:59:19 +0300

    004 task3 done

---
+++ 004/src/task3/Main.java
@@ -0,0 +1,37 @@
+package task3;
+
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime fromDateTime = LocalDateTime.of(1992, Calendar.MARCH, 22, 11, 1, 1);
+        LocalDateTime toDateTime = LocalDateTime.now();
+        getTimeBetweenTwoDates(fromDateTime, toDateTime);
+    }
+
+    private static void getTimeBetweenTwoDates(LocalDateTime fromDateTime, LocalDateTime toDateTime) {
+        Period period = Period.between(LocalDate.from(fromDateTime), LocalDate.from(toDateTime));
+        Duration duration = Duration.between(fromDateTime, toDateTime);
+        int hours;
+        int minutes;
+        int seconds;
+        if (LocalTime.from(fromDateTime).compareTo(LocalTime.from(toDateTime)) <= 0) {
+            hours = duration.toHoursPart();
+            minutes = duration.toMinutesPart();
+            seconds = duration.toSecondsPart();
+        } else {
+            period = period.minusDays(1);
+            hours = duration.toHoursPart();
+            minutes = duration.toMinutesPart();
+            seconds = duration.toSecondsPart();
+        }
+        int years = period.getYears();
+        int months = period.getMonths();
+        int days = period.getDays();
+
+
+        System.out.printf("Вам исполнилось %d лет, %d месяца, %d дней, %d часа, %d минут и %d секунд\n", years, months, days, hours, minutes, seconds);
+    }
+}
