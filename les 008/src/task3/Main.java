227325b419cbdf82c80f7b4f6e25f58eb6deba7b
Volodymyr <bugsbunny1163@gmail.com>
2022-07-04 11:55:55 +0300

    008 task 3 fix

--- les 008/src/task3/Main.java
+++ les 008/src/task3/Main.java
@@ -6,6 +6,7 @@ public class Main {
         priorityRunner.setPriority(Thread.MIN_PRIORITY);
         PriorityThread priorityThread = new PriorityThread();
         priorityThread.setPriority(Thread.MAX_PRIORITY);
+        Thread.currentThread().setPriority(Thread.NORM_PRIORITY);
         priorityRunner.start();
         priorityThread.start();
         for (int i = 0; i < 50; i++) {
--- les 008/src/task4/Main.java
+++ les 008/src/task4/Main.java
@@ -1,5 +1,23 @@
 package task4;
 
 public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        a.start();
+        b.start();
+    }
+}
 
+
+class A extends Thread {
+    @Override
+    public void run() {
+    }
+}
+
+class B extends Thread {
+    @Override
+    public void run() {
+    }
 }
