4ed82d9f7faa4c812ce8afc6cf289800a9018bd0
Volodymyr <bugsbunny1163@gmail.com>
2022-07-05 19:26:49 +0300

    009 task 4 done

---
+++ les 009/src/task4/Main.java
@@ -0,0 +1,26 @@
+package task4;
+
+public class Main {
+    public static void main(String[] args) {
+//        int a;
+//        int b;
+        Calculator summa;
+        summa = (a, b) -> a + b;
+        Calculator diff;
+        diff = (a, b) -> (a - b);
+        Calculator mul;
+        mul = (a, b) -> (a * b);
+        Calculator div;
+        div = (a, b) -> (a / b);
+
+
+        System.out.println(summa.calculating(1, 4));
+        System.out.println(diff.calculating(5, 1));
+        System.out.println(mul.calculating(5, 3));
+        System.out.println(div.calculating(6, 2));
+    }
+}
+
+interface Calculator {
+    int calculating(int a, int b);
+}
