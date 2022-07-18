8be1b09e84952d86e456fc0e2fa8431e8341a42a
Volodymyr <bugsbunny1163@gmail.com>
2022-07-18 14:30:20 +0300

    007 task4 done

--- les008/src/task4/Main.java
+++ les008/src/task4/Main.java
@@ -1,33 +1,69 @@
 package task4;
 
 public class Main {
-    //todo трохи не правильно зроблено, переробити
+    static volatile boolean lock;
 
     public static void main(String[] args) {
+        lock = false;
         A a = new A();
         B b = new B();
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-                    a.methodA();
-                    b.methodB();
+        Thread threadA = new Thread(a);
+        Thread threadB = new Thread(b);
+        threadA.start();
+        threadB.start();
+    }
+}
+
+class A implements Runnable {
+//    boolean lock;
+
+    public A() {
+//        this.lock = lock;
+    }
+
+    void methodA() {
+        while (true) {
+            if (!Main.lock) {
+                System.out.println(Thread.currentThread().getName() + " A class");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
+                Main.lock = true;
             }
-        });
-        thread.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        methodA();
     }
 }
 
+class B implements Runnable {
+//    boolean lock;
 
-class A {
-    public void methodA() {
-        System.out.println("A");
+    public B() {
+//        this.lock = lock;
     }
-}
 
-class B {
-    public void methodB() {
-        System.out.println("B");
+    void methodB() {
+        while (true) {
+            if (Main.lock) {
+                System.out.println(Thread.currentThread().getName() + " B class");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                Main.lock = false;
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        methodB();
     }
 }
