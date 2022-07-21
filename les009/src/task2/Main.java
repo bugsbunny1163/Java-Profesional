f78037deac5d8d2073163805aedc6fd692b9a596
Volodymyr <bugsbunny1163@gmail.com>
2022-07-21 16:22:43 +0300

    009 task2 done

--- les009/src/task2/Main.java
+++ les009/src/task2/Main.java
@@ -1,6 +1,17 @@
 package task2;
 
+import java.util.*;
+
 public class Main {
-    //todo зробити
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            list.add(random.nextInt(50));
+        }
+        System.out.println(list);
+        Optional<Integer> reduce = list.stream().map(x -> x * x).reduce(Integer::sum);
+        System.out.println(reduce.get());
+    }
 
 }
